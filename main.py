862313572cafaa5a2a6e141b0986154e144bd772
arpan0073 <arpand937@gmail.com>
2026-01-13 08:21:03 +0530

    Add files via upload
    Add Python script for converting Fahrenheit to Celsius using user input

---
+++ main.py
@@ -0,0 +1,8 @@
+# Fahrenheit to Celsius conversion
+
+f= float(input("Enter temperature in Fahrenheit: "))
+
+c = (f - 32) * 5 / 9
+
+print("Temperature in Celsius:", c)
+#Add i am leaving it again without full code,later i will add few things like what if users want to see celsius to farenheit?and what if they want to see it again and again?
